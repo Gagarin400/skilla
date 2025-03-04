0e2504ba75d0295ed80d7fe95cb1e58e0055ce58
gagarin40000 <Gagarin40000@gmail.com>
2025-03-04 19:10:21 +0300

    fix app in main

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import App from './App';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import '@qpokychuk/sf-pro-display/index.css';
 import '@qpokychuk/sf-pro-display/normal.css';
